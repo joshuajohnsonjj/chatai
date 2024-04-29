f4ce02424f16e0a758b103397d34ae28684bc4d8
Joshua Johnson <joshuajohnsonjj38@gmail.com>
2024-04-28 22:45:33 -0700

    start settings - wip

--- frontends/web-app/src/views/Settings.vue
+++ frontends/web-app/src/views/Settings.vue
@@ -1,1 +1,18 @@
-<template>Settings</template>
+<template>
+    <div class="bg-surface w-100 h-100 rounded-xl">
+        <div class="w-100 bottom-border-primary px-6 py-6 pb-4">
+            <p class="text-h4 font-weight-medium text-primary">Profile</p>
+            <p class="text-body-1 text-primary">Update your profile</p>
+        </div>
+
+        <v-row class="border-primary rounded pa-6">
+            <v-col cols="3">
+                <div class="text-h6 font-weight-medium text-primary">Profile photo</div>
+                <div class="text-body-2 text-secondary">This image will be displayed on your profile.</div>
+            </v-col>
+            <v-col cols="9">
+                <div class="border-primary rounded pa-6 bg-surface-bright">IMAGE DROPPER</div>
+            </v-col>
+        </v-row>
+    </div>
+</template>
